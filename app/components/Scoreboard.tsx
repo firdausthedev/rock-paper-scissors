1dfc323314d8c7bfd02639542ce84d8a0c68bb53
firdaus <firdausthedev@gmail.com>
2023-11-04 19:37:54 +0800

    scoreboard

---
+++ app/components/Scoreboard.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import Logo from "../assets/images/logo-bonus.svg";
+
+const Scoreboard = () => {
+  return (
+    <section className="pt-8 md:pt-[2.9rem] px-8 z-50">
+      <header
+        className="mx-auto max-w-[701px] flex items-center justify-between rounded-lg md:rounded-[1rem] border-2
+          border-headerOutline p-3 pl-4 md:px-6 md:pb-[0.8rem] md:pt-[0.9rem]">
+        <Image
+          width={0}
+          height={0}
+          alt="logo"
+          src={Logo}
+          className="h-[57px] w-[57px] md:h-[117px] md:w-[117px] pl-1 pt-1"
+        />
+        <div
+          className="flex w-fit px-5 h-[4.5rem] flex-col items-center justify-center rounded-lg bg-white py-3 
+              md:h-[113px] md:min-w-[150px]">
+          <p className="text-sm tracking-widest text-scoreText md:text-base">
+            SCORE
+          </p>
+          <p className="text-[3rem] leading-none -mt-1 font-bold tracking-widest text-darkText md:text-6xl">
+            0
+          </p>
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Scoreboard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Scoreboard from "./components/Scoreboard";
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradientBg relative flex flex-col"></main>
+    <main className="min-h-screen bg-gradientBg relative flex flex-col">
+      <Scoreboard />
+    </main>
   );
 }
